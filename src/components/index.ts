cbb5ed0614e688de04a4cfdd756946b7d192ad59
admindhaamai <admin@dhaamai.com>
2026-02-13 16:38:14 +0530

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 // Components barrel export
 export { default as AgentTable } from './AgentTable';
 export type { AgentTableProps, Agent } from './AgentTable';
-
 export { default as ReusableTable } from './ReusableTable';
 export type { GenericTableProps, TableColumn, TableRow } from './ReusableTable';
+export { default as ResponsiveAgentTable } from './ResponsiveAgentTable';
+export { default as FilterDropdown } from './FilterDropdown';
